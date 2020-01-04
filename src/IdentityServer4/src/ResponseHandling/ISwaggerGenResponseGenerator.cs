6435bd7830d658170ac9f1ca4184356d44cae3d8
amadard <adam.ard@gmail.com>
2020-01-03 21:55:06 -0800

    Adding ISwaggerGenResponseGenerator for generating OpenApiDocument that is consumed by SwaggerGenResult

---
+++ src/IdentityServer4/src/ResponseHandling/ISwaggerGenResponseGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.ResponseHandling
+{
+    /// <summary>
+    /// Interface for Swagger Gen OpenAPIDocument generation
+    /// </summary>
+    public interface ISwaggerGenResponseGenerator
+    {
+        /// <summary>
+        /// Creates an OpenApiDocument for Swagger
+        /// </summary>
+        /// <param name="issuerUri">Base URI for IdentityServer</param>
+        Task<OpenApiDocument> CreateSwaggerDocAsync(string issuerUri);
+    }
+}
